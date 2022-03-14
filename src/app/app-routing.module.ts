0260c964cf94b3747cb4897fad21f444feaf8b55
Vivian <vivianleitef@gmail.com>
2022-03-14 15:07:53 -0300

    Projeto final

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas/tarefas-routing.module';
+
+export const routes: Routes = [
+
+  {
+      path:'',
+      redirectTo: '/tarefas/listar',
+      pathMatch: 'full'
+  },
+
+  ...TarefaRoutes
+	
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
